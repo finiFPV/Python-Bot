6618a3f48cd5f0dba11f3ecf0f5b9501aca1decd
fininimini <markusskalenda@gmail.com>
2022-11-28 20:15:49 +0200

    Moved environment variable to data.py

---
+++ data.py
@@ -0,0 +1,5 @@
+class Data:
+    def owner(interaction): return interaction.user.id == 973604858764591145
+    rep_category = 'Private Channels'
+    rep_channel = '➕ Create private channel'
+    botToken='MTA0NjE3ODQ5MzAzNTE4NDIzMQ.Gc2Me2.I43BKpOGqk5T7GWd0wanNDp_iBaxrO6m230eb4'
