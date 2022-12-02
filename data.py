05d5888f0e955ce6fe44926e5ba425ce4e6dad03
fininimini <markusskalenda@gmail.com>
2022-12-02 22:54:12 +0200

    More audio, improovments

--- data.py
+++ data.py
@@ -1,7 +1,12 @@
 class Data:
-    owner = ***REMOVED***
-    authorized = [***REMOVED***, ***REMOVED***, ***REMOVED***]
-    uno_reversed_users = []#***REMOVED***, ***REMOVED***]
-    rep_category = 'Private Channels'
-    rep_channel = '➕ Create private channel'
-    botToken='***REMOVED***'
+    class Id:
+        owner = ***REMOVED***
+        uno_reversed_users = []#***REMOVED***, ***REMOVED***]
+        authorized = [***REMOVED***, ***REMOVED***, ***REMOVED***]
+    class Private_Channel:
+        channels = []
+        category = 'Private Channels'
+        channel = '➕ Create private channel'
+    class Other:
+        music_enabled = True
+        bot_token='***REMOVED***'
