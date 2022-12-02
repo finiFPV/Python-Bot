32bb34ff957eb9edfbc81622e23773c858a7b9af
fininimini <markusskalenda@gmail.com>
2022-12-02 23:44:13 +0200

    Setup for merge with main

--- data.py
+++ data.py
@@ -1,12 +1,12 @@
 class Data:
     class Id:
         owner = 973604858764591145
-        uno_reversed_users = []#533345538242773027, 760109835693326356]
+        uno_reversed_users = [533345538242773027, 760109835693326356]
         authorized = [852662926641070110, 533345538242773027, 760109835693326356]
     class Private_Channel:
         channels = []
         category = 'Private Channels'
         channel = '➕ Create private channel'
     class Other:
-        music_enabled = True
+        music_enabled = False
         bot_token='MTA0NjE3ODQ5MzAzNTE4NDIzMQ.Gc2Me2.I43BKpOGqk5T7GWd0wanNDp_iBaxrO6m230eb4'
